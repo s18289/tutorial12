00c67aa9fbb8f8fe819a1a1f76e7c986066d39b9
s18289 <s18289@pjwstk.edu.pl>
2020-06-05 13:24:49 +0200

    commit

---
+++ tutorial12/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tutorial12.Models;
+
+namespace tutorial12.Configurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasKey(e => e.IdEmployee)
+                  .HasName("Impl_pk");
+
+            builder.Property(e => e.Name)
+                  .IsRequired()
+                  .HasMaxLength(50);
+
+            builder.Property(e => e.Surname)
+                      .IsRequired()
+                      .HasMaxLength(60);
+        }
+    }
+}
--- tutorial12/Configurations/OrderConfiguration.cs
+++ tutorial12/Configurations/OrderConfiguration.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using tutorial12.Models;
 
 namespace tutorial12.Configurations
 {
-    public class OrderConfiguration
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
     {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(e => e.IdOrder)
+                  .HasName("Order_pk");
 
+            builder.Property(e => e.DateAccepted).HasColumnType("date")
+                  .IsRequired();
+
+
+            builder.Property(e => e.DateFinished)
+                      .IsRequired();
+
+            builder.Property(e => e.Notes).IsRequired().HasMaxLength(255);
+
+        }
     }
 }
